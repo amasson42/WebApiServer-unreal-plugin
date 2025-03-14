f1f0a525264bddd3e7d156be7c8ccfc72ab6c8d8
amasson <arthur.masson.3896@gmail.com>
2025-03-14 19:54:17 +0100

    Handle dynamic event handlers for request and notifications

--- Source/WebApiServer/Private/Dispatcher/JsonMessageDispatcher.cpp
+++ Source/WebApiServer/Private/Dispatcher/JsonMessageDispatcher.cpp
@@ -3,35 +3,52 @@
 
 #include "Dispatcher/JsonMessageDispatcher.h"
 
-
-bool UJsonMessageDispatcher::RegisterRequestHandler(const FString& Method, const FJsonRequestHandler& Handler, bool bOverride)
+bool UJsonMessageDispatcher::RegisterRequestHandler(const FString& Method, const FJsonRequestHandlerDelegate& Handler, bool bOverride)
 {
+        if (!bOverride)
+    {
+        auto* CurrentHandlerPtr = RequestHandlers.Find(Method);
+        if (CurrentHandlerPtr && CurrentHandlerPtr->IsValid())
+            return false;
+    }
+
+    auto NewHandler = MakeShared<FJsonRequestHandlerWithDelegate>();
+    NewHandler->Delegate = Handler;
+
+    RequestHandlers.Add(Method, NewHandler);
+    return true;
+}
+
+bool UJsonMessageDispatcher::IsRequestHandlerRegistered(const FString& Method, const FJsonRequestHandlerDelegate& Handler) const
+{
+    // TODO: Check if of correct type and correct handler
     return false;
 }
 
-bool UJsonMessageDispatcher::IsRequestHandlerRegistered(const FString& Method, const FJsonRequestHandler& Handler) const
+bool UJsonMessageDispatcher::UnregisterRequestHandler(const FString& Method, const FJsonRequestHandlerDelegate& Handler)
 {
+    if (IsRequestHandlerRegistered(Method, Handler))
+    {
+        RequestHandlers.Remove(Method);
+        return true;
+    }
     return false;
 }
 
-bool UJsonMessageDispatcher::UnregisterRequestHandler(const FString& Method, const FJsonRequestHandler& Handler)
+void UJsonMessageDispatcher::RegisterNotificationCallback(const FString& Method, const FJsonNotificationHandlerDelegate& Callback)
 {
-    return false;
+    auto NewHandler = MakeShared<FJsonNotificationHandlerWithDelegate>();
+    NewHandler->Delegate = Callback;
+
+    if (TArray<TSharedPtr<FJsonNotificationHandler>>* MethodHandlers = NotificationHandlers.Find(Method))
+        MethodHandlers->Add(NewHandler);
+    else
+        NotificationHandlers.Add(Method, {NewHandler});
 }
 
-void UJsonMessageDispatcher::RegisterNotificationCallback(const FString& Method, const FJsonNotificationCallback& Callback)
+void UJsonMessageDispatcher::UnregisterNotificationCallback(const FString& Method, const FJsonNotificationHandlerDelegate& Callback)
 {
-
-}
-
-bool UJsonMessageDispatcher::IsNotificationCallbackRegistered(const FString& Method, const FJsonNotificationCallback& Callback) const
-{
-    return false;
-}
-
-void UJsonMessageDispatcher::UnregisterNotificationCallback(const FString& Method, const FJsonNotificationCallback& Callback)
-{
-
+    // TODO: Filter out all matching notification handlers
 }
 
 
@@ -39,12 +56,86 @@ void UJsonMessageDispatcher::UnregisterNotificationCallback(const FString& Metho
 
 void UJsonMessageDispatcher::HandleMessage(const FString& Message, TScriptInterface<IMessageSender> MessageSender)
 {
-    // TODO:
-    // Add parameter IMessageCallback that can send a message as a response
-    // 
+    TSharedPtr<FJsonObject> JsonMessage;
+    TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Message);
+
+    if (!FJsonSerializer::Deserialize(Reader, JsonMessage) || !JsonMessage.IsValid())
+    {
+        MessageSender->SendMessage(TEXT("invalid_json"));
+        return;
+    }
+
+    HandleJsonMessage(JsonMessage, MessageSender);
 }
 
 void UJsonMessageDispatcher::HandleJsonMessage(const FJsonObjectWrapper& JsonMessage, TScriptInterface<IMessageSender> MessageSender)
 {
+    HandleJsonMessage(JsonMessage.JsonObject, MessageSender);
+}
 
+void SendJsonResponse(const TSharedPtr<FJsonObject>& JsonResponse, TScriptInterface<IMessageSender> MessageSender)
+{
+    FString StringResponse;
+    TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&StringResponse);
+
+    if (!FJsonSerializer::Serialize(JsonResponse.ToSharedRef(), Writer))
+    {
+        MessageSender->SendMessage(TEXT("internal_serialization_error"));
+        return;
+    }
+
+    MessageSender->SendMessage(StringResponse);
+}
+
+TSharedPtr<FJsonObject> MakeErrorJson(int32 Id, const FString &Error)
+{
+    TSharedPtr<FJsonObject> ErrorJson = MakeShared<FJsonObject>();
+
+    ErrorJson->SetNumberField(TEXT(JSONRPC_ID), Id);
+    ErrorJson->SetStringField(TEXT(JSONRPC_ERROR), Error);
+    return ErrorJson;
+}
+
+void UJsonMessageDispatcher::HandleJsonMessage(const TSharedPtr<FJsonObject>& JsonMessage, TScriptInterface<IMessageSender> MessageSender)
+{
+    int32 Id;
+    bool bShouldReturn = JsonMessage->TryGetNumberField(TEXT(JSONRPC_ID), Id);
+
+    FString Method;
+    if (!JsonMessage->TryGetStringField(TEXT(JSONRPC_METHOD), Method))
+    {
+        if (bShouldReturn)
+            SendJsonResponse(MakeErrorJson(Id, TEXT("no method in request")), MessageSender);
+        return;
+    }
+
+    if (bShouldReturn)
+    {
+        TSharedPtr<FJsonRequestHandler>* Handler = RequestHandlers.Find(Method);
+        if (Handler == nullptr || !Handler->IsValid())
+            return SendJsonResponse(MakeErrorJson(Id, FString::Printf(TEXT("no handlers for method %s"), *Method)), MessageSender);
+
+        (*Handler)->HandleRequest(JsonMessage->TryGetField(TEXT(JSONRPC_PARAMS)),
+            [Id, MessageSender](const TSharedPtr<FJsonValue>& Response)
+            {
+            TSharedPtr<FJsonObject> JsonResponse = MakeShared<FJsonObject>();
+            JsonResponse->SetNumberField(TEXT(JSONRPC_ID), Id);
+                JsonResponse->SetObjectField(TEXT(JSONRPC_RESULT), Response->AsObject());
+                SendJsonResponse(JsonResponse, MessageSender);
+            }, [Id, MessageSender](const FString& Error)
+            {
+                SendJsonResponse(MakeErrorJson(Id, Error), MessageSender);
+            });
+    }
+    else
+    {
+        auto* Handlers = NotificationHandlers.Find(Method);
+        if (!Handlers)
+            return;
+
+        for (const auto& Handler : *Handlers)
+        {
+            Handler->HandleNotification(JsonMessage->TryGetField(TEXT(JSONRPC_PARAMS)));
+        }
+    }
 }
--- Source/WebApiServer/Public/Dispatcher/JsonMessageDispatcher.h
+++ Source/WebApiServer/Public/Dispatcher/JsonMessageDispatcher.h
@@ -9,8 +9,97 @@
 #include "Messaging/MessageSender.h"
 #include "JsonMessageDispatcher.generated.h"
 
-DECLARE_DYNAMIC_DELEGATE_TwoParams(FJsonRequestHandler, FJsonObjectWrapper, Params, FJsonObjectWrapper&, Result);
-DECLARE_DYNAMIC_DELEGATE_OneParam(FJsonNotificationCallback, FJsonObjectWrapper, Params);
+#define JSONRPC_ID "id"
+#define JSONRPC_METHOD "method"
+#define JSONRPC_PARAMS "params"
+#define JSONRPC_RESULT "result"
+#define JSONRPC_ERROR "error"
+
+DECLARE_DYNAMIC_DELEGATE_FourParams(FJsonRequestHandlerDelegate, FJsonObjectWrapper, Params, FJsonObjectWrapper&, Result, FString&, Error, bool&, Success);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FJsonNotificationHandlerDelegate, FJsonObjectWrapper, Params);
+
+// TODO: Put Handlers structures in private files
+
+/** Abstract RequestHandler class */
+typedef TFunction<void (const TSharedPtr<FJsonValue>&)> JsonRequestCompletionCallback;
+typedef TFunction<void (const FString&)> JsonRequestErrorCallback;
+
+USTRUCT()
+struct FJsonRequestHandler
+{
+    GENERATED_BODY()
+
+    virtual ~FJsonRequestHandler() {}
+
+    virtual void HandleRequest(
+        const TSharedPtr<FJsonValue>& Param,
+        const JsonRequestCompletionCallback& Completion,
+        const JsonRequestErrorCallback& Error)
+    {}
+};
+
+/** RequestHandler using Delegate */
+USTRUCT()
+struct FJsonRequestHandlerWithDelegate : public FJsonRequestHandler
+{
+    GENERATED_BODY()
+
+    virtual ~FJsonRequestHandlerWithDelegate() override {}
+
+    virtual void HandleRequest(
+        const TSharedPtr<FJsonValue>& Param,
+        const JsonRequestCompletionCallback& Completion,
+        const JsonRequestErrorCallback& Error
+    ) override
+    {
+        FJsonObjectWrapper ParamWrapper;
+        if (Param.IsValid())
+            ParamWrapper.JsonObject = Param->AsObject();
+        FJsonObjectWrapper ResultWrapper;
+        FString ErrorMessage;
+        bool Success;
+        Delegate.ExecuteIfBound(ParamWrapper, ResultWrapper, ErrorMessage, Success);
+        if (Success)
+            Completion(MakeShared<FJsonValueObject>(ResultWrapper.JsonObject));
+        else
+            Error(ErrorMessage);
+    }
+
+    FJsonRequestHandlerDelegate Delegate;
+};
+
+
+USTRUCT()
+struct FJsonNotificationHandler
+{
+    GENERATED_BODY()
+
+    virtual ~FJsonNotificationHandler() {}
+
+    virtual void HandleNotification(const TSharedPtr<FJsonValue>& Param) {}
+};
+
+/** RequestHandler using Delegate */
+USTRUCT()
+struct FJsonNotificationHandlerWithDelegate : public FJsonNotificationHandler
+{
+    GENERATED_BODY()
+
+    virtual ~FJsonNotificationHandlerWithDelegate() override {}
+
+    virtual void HandleNotification(const TSharedPtr<FJsonValue>& Param) override
+    {
+        FJsonObjectWrapper ParamWrapper;
+        if (Param.IsValid())
+            ParamWrapper.JsonObject = Param->AsObject();
+        (void)Delegate.ExecuteIfBound(ParamWrapper);
+    }
+
+    FJsonNotificationHandlerDelegate Delegate;
+};
+
+
+/** </Handlers> */
 
 /**
  * 
@@ -24,27 +113,23 @@ public:
 
     /** Register a request handler. Request handlers are unique per methods. */
     UFUNCTION(BlueprintCallable, Category = "Handler|Request")
-    bool RegisterRequestHandler(const FString& Method, const FJsonRequestHandler& Handler, bool bOverride = false);
+    bool RegisterRequestHandler(const FString& Method, const FJsonRequestHandlerDelegate& Handler, bool bOverride = false);
 
     /** Check if a request handler is registered */
     UFUNCTION(BlueprintCallable, Category = "Handler|Request")
-    bool IsRequestHandlerRegistered(const FString& Method, const FJsonRequestHandler& Handler) const;
+    bool IsRequestHandlerRegistered(const FString& Method, const FJsonRequestHandlerDelegate& Handler) const;
 
     /** Unregister a request handler */
     UFUNCTION(BlueprintCallable, Category = "Handler|Request")
-    bool UnregisterRequestHandler(const FString& Method, const FJsonRequestHandler& Handler);
+    bool UnregisterRequestHandler(const FString& Method, const FJsonRequestHandlerDelegate& Handler);
 
     /** Register a notification callback. Notification callbacks are not unique per methods. */
     UFUNCTION(BlueprintCallable, Category = "Handler|Notification")
-    void RegisterNotificationCallback(const FString& Method, const FJsonNotificationCallback& Callback);
-
-    /** Check if a notification callback is registered */
-    UFUNCTION(BlueprintCallable, Category = "Handler|Notification")
-    bool IsNotificationCallbackRegistered(const FString& Method, const FJsonNotificationCallback& Callback) const;
+    void RegisterNotificationCallback(const FString& Method, const FJsonNotificationHandlerDelegate& Callback);
 
     /** Unregister a notification h */
     UFUNCTION(BlueprintCallable, Category = "Handler|Notification")
-    void UnregisterNotificationCallback(const FString& Method, const FJsonNotificationCallback& Callback);
+    void UnregisterNotificationCallback(const FString& Method, const FJsonNotificationHandlerDelegate& Callback);
 
 
     /** Message handling */
@@ -55,12 +140,11 @@ public:
     UFUNCTION(BlueprintCallable)
     void HandleJsonMessage(const FJsonObjectWrapper& JsonMessage, TScriptInterface<IMessageSender> MessageSender);
 
+    void HandleJsonMessage(const TSharedPtr<FJsonObject>& JsonMessage, TScriptInterface<IMessageSender> MessageSender);
+
 private:
 
-    UPROPERTY()
-    TMap<FString, FJsonRequestHandler> RequestHandlers;
-
-    UPROPERTY()
-    TMap<FString, FJsonNotificationCallback> NotificationCallbacks;
+    TMap<FString, TSharedPtr<FJsonRequestHandler>> RequestHandlers;
+    TMap<FString, TArray<TSharedPtr<FJsonNotificationHandler>>> NotificationHandlers;
 
 };
